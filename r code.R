800ad528a3b7ff4c0c6071d3635616cf9c835c58
Sefa Ozalp <sefabey@gmail.com>
2017-04-19 01:39:30 +0100

    Add files via upload

---
+++ r code.R
@@ -0,0 +1,5 @@
+tmp = installed.packages()
+installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+save(installedpackages, file="~/Desktop/installed_packages.rda")
+load("~/Desktop/installed_packages.rda")
+for (count in 1:length(installedpackages)) install.packages(installedpackages[count])
